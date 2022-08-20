5b0d22437c4794480c76da942c6c9b37014cd06f
翁松青 <1310207176@qq.com>
2022-08-20 17:02:37 +0800

    添加工具类

---
+++ utils/tools.js
@@ -0,0 +1,22 @@
+module.exports = {
+  ArrayISEmpty(array) {
+    return array.length === 0
+  },
+  //生成几位数的随机数
+  createRandomNum(n) {
+    let res = ''
+    for (let i = 0; i < n; i++) {
+      res += ~~(Math.random() * 9)
+    }
+    return res
+  },
+  time13TransferTime10(time) {
+    return Math.round(time / 1000)
+  },
+  createRandomNumWidthScope(min, max) {
+    return ~~(Math.random() * (max - min) + min)
+  },
+  objectISEmpty(obj) {
+    return Object.keys(obj).length > 0
+  },
+}
