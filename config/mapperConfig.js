cb1f059559dc722b33a7ae404d68c2af13a48033
翁松青 <1310207176@qq.com>
2022-08-20 15:54:23 +0800

    全局映射文件修改有错误

--- config/mapperConfig.js
+++ config/mapperConfig.js
@@ -1,12 +1,12 @@
 const User = require('../db/models/user')
 const Garbage=require('../db/models/garbage')
 const GarbageCategory=require('../db/models/garbage_category')
-const KeyWords=require('../db/models/keywords')
-const KeyWordsNum=require('../db/models/keywords_num')
+const Keywords=require('../db/models/keywords')
+const KeywordsNum=require('../db/models/keywords_num')
 module.exports = {
     User,
     Garbage,
     GarbageCategory,
-    KeyWords,
-    KeyWordsNum
+    Keywords,
+    KeywordsNum
 }
